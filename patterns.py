64b8d531f01fd22f1bfff0910a73b29077dedc87
prasahant <prashantnagasai@gmail.com>
2025-07-25 16:33:50 +0530

    programs

---
+++ patterns.py
@@ -0,0 +1,9 @@
+#printing the Hallow square
+n=int(input("enter the size of the square:"))
+for i in range(n):
+    for j in range(n):
+        if j==0 or j==n-1 or i==0  or i==n-1 :
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
