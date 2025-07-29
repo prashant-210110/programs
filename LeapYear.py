d6499b94a2eca986655d89751da2cafaa11f3c9b
prashant-210110 <prashantnagasai@gmail.com>
2025-07-29 21:32:57 +0530

    Add files via upload

---
+++ LeapYear.py
@@ -0,0 +1,17 @@
+#chechk the given year is leap year not
+#9603526342
+x=int(input("enter the year"))
+b=len(str(x))
+if b==4:
+    if x%4==0:
+        if x%100==0:
+            if x%400==0:
+                print("leap year")
+            else:
+                print("not a leap year")
+        else:
+            print("leap year")
+    else:
+        print("not a leap year")
+else:
+    print("enter the 4 digit year")
