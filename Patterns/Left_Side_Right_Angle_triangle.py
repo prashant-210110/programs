70037c2db069c36ca2cd28fc9028bb710bfc0427
prashantgit config --global user.email prashantnagasai@gmail.com <prashantnagasai@gmail.com>
2025-08-12 16:22:14 +0530

    Update Left_Side_Right_Angle_triangle.py

--- Patterns/Left_Side_Right_Angle_triangle.py
+++ Patterns/Left_Side_Right_Angle_triangle.py
@@ -1,6 +1,6 @@
 #left sided right angle triangle
- n=int(input("enter the size"))
- for i in range(0,n+1):
+n=int(input("enter the size"))
+for i in range(0,n+1):
   for j in range(i,n+1):
     print(" ",end=" ")
   for j in range(i):
