1d77ac462b4ffd5fca142a7f1574bcdd89be923e
prashant-210110 <prashantnagasai@gmail.com>
2025-08-20 21:21:51 +0530

    Update Prime_Number.py

--- Prime_Number.py
+++ Prime_Number.py
@@ -14,13 +14,18 @@ for i in range(start,end+1):              # Loop through numbers from 0 to 99
 
 
 #checking the given number is prime or not
-n = int(input("enter the number"))     # Take input from the user and convert it to an integer
-if n > 1 :                             # Prime numbers are greater than 1
-    for i in range(2, n):              # Loop from 2 to n-1 to check for any divisors
-        if n % i == 0:                 # If n is divisible by i, it's not prime
-            print("not prime")         # Print result if a divisor is found
-            break                      # Exit the loop early since we know it's not prime
-    else:                              # This else belongs to the for-loop, not the if-statement
-        print("prime")                 # If no divisors were found, n is prime
-else:                                  #else block
-    print("not prime")                 # If n is 1 or less, it's not prime
+#program to check the given number is prime number or not
+n=int(input("enter the number"))
+if n<=0:
+  print("enter valide number")
+elif n==1:
+  print("1 is not a prime number")
+elif n==2:
+  print("2 is only even prime number")
+else:
+  for i in range(2,n):
+    if n%i==0:
+      print(f"{n} is not prime number")
+      break
+  else:
+    print(f"{n} is a prime number")
