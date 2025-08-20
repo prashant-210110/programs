110f425549c53cc6661ee5a5b36e806420bc42bb
prashant-210110 <prashantnagasai@gmail.com>
2025-08-20 21:14:44 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,7 +1,7 @@
 #checking the given strong is palindrome or not
 a=input("enter the string")
-b=a[::-1]
-if a==b:
-  print(f"{a} is palindrome")
+b=a[::-1]                           #reversing the string by using slicing
+if a==b:                            #checking if the both strings are equal or not
+  print(f"{a} is palindrome")       #if equal this will print
 else:
-  print(f"{a} is not a palindrome")
+  print(f"{a} is not a palindrome") #else this will print
