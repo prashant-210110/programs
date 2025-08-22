d4ea41fcf958eb60ba99c108bbc3827936959d87
prashant-210110 <prashantnagasai@gmail.com>
2025-08-22 22:46:36 +0530

    Update Daimond.py

--- Patterns/Daimond.py
+++ Patterns/Daimond.py
@@ -30,3 +30,26 @@ for i in range(1, n+1):
   print(" "*(n-i) ,"* "*i)
 for i in range(n-1,0,-1):
   print(" "*(n-i),"* "*i)
+
+
+
+
+#daimond program
+n = int(input("enter the size of pyramid"))    # Ask user for the size (height) of the pyramid
+
+# 🔼 Upper Half of the Diamond (Growing Pyramid)
+for i in range(n):                            
+    for j in range(i, n):                      # Print decreasing spaces to center the stars
+        print(" ", end="")                    
+    for j in range(i):                         # Print increasing stars in each row
+        print("*", end=" ")                   
+    print()                                    # Move to the next line
+
+# 🔽 Lower Half of the Diamond (Shrinking Pyramid)
+for i in range(n):                            
+    for j in range(i):                         # Print increasing spaces to shift stars right
+        print(" ", end="")                    
+    for j in range(n, i, -1):                  # Print decreasing stars in each row
+        print("*", end=" ")                   
+    print()                                    # Move to the next line
+                                 
